e784886929f083ce8b1cd90275ec69393436643a
niranjannlc <77341018@nou.edu.np>
2023-03-14 12:04:20 +0545

    Modified the response body of http exception and added mock stub

--- app/src/commonTest/java/org/mifos/mobile/RetrofitUtils.kt
+++ app/src/commonTest/java/org/mifos/mobile/RetrofitUtils.kt
@@ -1,6 +1,7 @@
 package org.mifos.mobile
 
 import okhttp3.MediaType
+import okhttp3.Protocol
 import okhttp3.ResponseBody
 import retrofit2.HttpException
 import retrofit2.Response
@@ -10,10 +11,21 @@ import retrofit2.Response
  */
 object RetrofitUtils {
     fun get404Exception(): Exception {
-        return HttpException(Response.error<Any>(404, ResponseBody.create(MediaType.parse("application/json"), "Not Found")))
+        return HttpException(getResponseForError(404, "Not Found"))
     }
 
     fun get401Exception(): Exception {
-        return HttpException(Response.error<Any>(401, ResponseBody.create(MediaType.parse("application/json"), "UnAuthorized")))
+        return HttpException(getResponseForError(401, "Unauthorized"))
+    }
+
+    //generate response for error based on code and messaghe
+    private fun getResponseForError(errorCode: Int, message : String ): Response<Any> {
+        val responseBody = ResponseBody.create(MediaType.parse("application/json"), "{\"message\":\"$message\"}")
+        val response = okhttp3.Response.Builder().code(errorCode)
+            .message(message)
+            .protocol(Protocol.HTTP_1_1)
+            .request(okhttp3.Request.Builder().url("http://localhost/").build())
+            .build()
+        return Response.error(responseBody,response)
     }
 }
--- app/src/test/java/org/mifos/mobile/UpdatePasswordPresenterTest.kt
+++ app/src/test/java/org/mifos/mobile/UpdatePasswordPresenterTest.kt
@@ -54,6 +54,7 @@ class UpdatePasswordPresenterTest {
     fun setUp() {
         Mockito.`when`(preferencesHelper?.baseUrl)
                 .thenReturn(BaseURL.PROTOCOL_HTTPS + BaseURL.API_ENDPOINT)
+        Mockito.`when`(preferencesHelper?.userName).thenReturn("username") // username is required to update authentication token
         presenter = UpdatePasswordPresenter(context!!, dataManager!!, preferencesHelper!!)
         passwordPayload = FakeRemoteDataSource.updatePasswordPayload
         presenter?.attachView(view)
